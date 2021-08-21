0178b04c776c18338b422565a802d28300587818
AHpx <ahpx@yandex.com>
2021-08-21 22:55:49 +0800

    Accomplished home interface

--- YetAnotherMinecraftLauncher/ViewModels/MainWindowViewModel.cs
+++ YetAnotherMinecraftLauncher/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,45 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Avalonia.Media.Imaging;
+using ReactiveUI;
 
 namespace YetAnotherMinecraftLauncher.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
         public string Greeting => "Welcome to Avalonia!";
+
+        private Bitmap _versionAvatar = new(@"C:\Users\ahpx\Desktop\AHpxEx.png");
+        public Bitmap VersionAvatar
+        {
+            get => _versionAvatar;
+            set => this.RaiseAndSetIfChanged(ref _versionAvatar, value);
+        }
+
+        private Bitmap _accountAvatar = new(@"C:\Users\ahpx\Desktop\AHpxEx.png");
+
+        public Bitmap AccountAvatar
+        {
+            get => _accountAvatar;
+            set => this.RaiseAndSetIfChanged(ref _accountAvatar, value);
+        }
+
+        private string _accountName = "alwhdawdjklhjawkjdhawkdjh";
+
+        public string AccountName
+        {
+            get => _accountName;
+            set => this.RaiseAndSetIfChanged(ref _accountName, value);
+        }
+
+        private string _accountType = "Microsoft";
+
+        public string AccountType
+        {
+            get => _accountType;
+            set => this.RaiseAndSetIfChanged(ref _accountType, value);
+        }
 
         public void Test()
         {
